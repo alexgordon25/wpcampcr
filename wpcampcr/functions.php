19a7b7ce86c82500058a87b98ae48f1ea0664358
Daniel Gordon <alexgordon25@gmail.com>
2016-11-05 08:32:06 -0600

    adding readme and demo theme

--- wpcampcr/functions.php
+++ wpcampcr/functions.php
@@ -1,25 +1,145 @@
-# Creando Temas Personalizados para Clientes.
+<?php
+/**
+ * wpcampcr functions and definitions.
+ *
+ * @link https://developer.wordpress.org/themes/basics/theme-functions/
+ *
+ * @package wpcampcr
+ */
 
-## Caso de Ejemplo.
+if ( ! function_exists( 'wpcampcr_setup' ) ) :
+/**
+ * Sets up theme defaults and registers support for various WordPress features.
+ *
+ * Note that this function is hooked into the after_setup_theme hook, which
+ * runs before the init hook. The init hook is too late for some features, such
+ * as indicating support for post thumbnails.
+ */
+function wpcampcr_setup() {
+	/*
+	 * Make theme available for translation.
+	 * Translations can be filed in the /languages/ directory.
+	 * If you're building a theme based on wpcampcr, use a find and replace
+	 * to change 'wpcampcr' to the name of your theme in all the template files.
+	 */
+	load_theme_textdomain( 'wpcampcr', get_template_directory() . '/languages' );
 
-El Cliente requiere un sitio web con una sección principal de catálogo de Películas y otra de Noticias, el resto de paginas del sitio son estáticas.
+	// Add default posts and comments RSS feed links to head.
+	add_theme_support( 'automatic-feed-links' );
 
-PORTADA
-	- PELICULAS
-	- NOTICIAS
-	- ACERCA DE NOSOTROS
-	- CONTACTENOS
+	/*
+	 * Let WordPress manage the document title.
+	 * By adding theme support, we declare that this theme does not use a
+	 * hard-coded <title> tag in the document head, and expect WordPress to
+	 * provide it for us.
+	 */
+	add_theme_support( 'title-tag' );
 
-### Herramientas utilizadas.
-- [Tema base de Underscores](http://underscores.me/)
-- [WP Generate](https://generatewp.com/)
-- [Advanced Custom Fields](https://www.advancedcustomfields.com/)
+	/*
+	 * Enable support for Post Thumbnails on posts and pages.
+	 *
+	 * @link https://developer.wordpress.org/themes/functionality/featured-images-post-thumbnails/
+	 */
+	add_theme_support( 'post-thumbnails' );
 
-### Registro de "Custom Post Type" Película.
+	// This theme uses wp_nav_menu() in one location.
+	register_nav_menus( array(
+		'primary' => esc_html__( 'Primary', 'wpcampcr' ),
+	) );
 
-Agregar al final de function.php
+	/*
+	 * Switch default core markup for search form, comment form, and comments
+	 * to output valid HTML5.
+	 */
+	add_theme_support( 'html5', array(
+		'search-form',
+		'comment-form',
+		'comment-list',
+		'gallery',
+		'caption',
+	) );
 
-```php
+	// Set up the WordPress core custom background feature.
+	add_theme_support( 'custom-background', apply_filters( 'wpcampcr_custom_background_args', array(
+		'default-color' => 'ffffff',
+		'default-image' => '',
+	) ) );
+}
+endif;
+add_action( 'after_setup_theme', 'wpcampcr_setup' );
+
+/**
+ * Set the content width in pixels, based on the theme's design and stylesheet.
+ *
+ * Priority 0 to make it available to lower priority callbacks.
+ *
+ * @global int $content_width
+ */
+function wpcampcr_content_width() {
+	$GLOBALS['content_width'] = apply_filters( 'wpcampcr_content_width', 640 );
+}
+add_action( 'after_setup_theme', 'wpcampcr_content_width', 0 );
+
+/**
+ * Register widget area.
+ *
+ * @link https://developer.wordpress.org/themes/functionality/sidebars/#registering-a-sidebar
+ */
+function wpcampcr_widgets_init() {
+	register_sidebar( array(
+		'name'          => esc_html__( 'Sidebar', 'wpcampcr' ),
+		'id'            => 'sidebar-1',
+		'description'   => esc_html__( 'Add widgets here.', 'wpcampcr' ),
+		'before_widget' => '<section id="%1$s" class="widget %2$s">',
+		'after_widget'  => '</section>',
+		'before_title'  => '<h2 class="widget-title">',
+		'after_title'   => '</h2>',
+	) );
+}
+add_action( 'widgets_init', 'wpcampcr_widgets_init' );
+
+/**
+ * Enqueue scripts and styles.
+ */
+function wpcampcr_scripts() {
+	wp_enqueue_style( 'wpcampcr-style', get_stylesheet_uri() );
+
+	wp_enqueue_script( 'wpcampcr-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20151215', true );
+
+	wp_enqueue_script( 'wpcampcr-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20151215', true );
+
+	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
+		wp_enqueue_script( 'comment-reply' );
+	}
+}
+add_action( 'wp_enqueue_scripts', 'wpcampcr_scripts' );
+
+/**
+ * Implement the Custom Header feature.
+ */
+require get_template_directory() . '/inc/custom-header.php';
+
+/**
+ * Custom template tags for this theme.
+ */
+require get_template_directory() . '/inc/template-tags.php';
+
+/**
+ * Custom functions that act independently of the theme templates.
+ */
+require get_template_directory() . '/inc/extras.php';
+
+/**
+ * Customizer additions.
+ */
+require get_template_directory() . '/inc/customizer.php';
+
+/**
+ * Load Jetpack compatibility file.
+ */
+require get_template_directory() . '/inc/jetpack.php';
+
+// Register Custom Post Type
 function register_movie_post_type() {
 
 	$labels = array(
@@ -79,13 +199,8 @@ function register_movie_post_type() {
 
 }
 add_action( 'init', 'register_movie_post_type', 0 );
-```
 
-### Registro de "Custom Taxonomy" Género
-
-Agregar al final de function.php
-
-```php
+// Register Custom Taxonomy
 function register_genre_taxonomy() {
 
 	$labels = array(
@@ -117,7 +232,7 @@ function register_genre_taxonomy() {
 	);
 	$args = array(
 		'labels'                     => $labels,
-		'hierarchical'               => false,
+		'hierarchical'               => true,
 		'public'                     => true,
 		'show_ui'                    => true,
 		'show_admin_column'          => true,
@@ -129,4 +244,6 @@ function register_genre_taxonomy() {
 
 }
 add_action( 'init', 'register_genre_taxonomy', 0 );
-```
+
+// Remove Welcome Panel
+remove_action('welcome_panel', 'wp_welcome_panel');
